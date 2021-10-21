407a6e89e56e6bc8e072a2a1ad3c2513564f131f
hasan demir <65541711+hasandmr@users.noreply.github.com>
2021-10-21 12:58:22 +0300

    Add files via upload
    belli karakterleri referans alaraktan 5 haneli random 1000 adet şifre oluşturan bruteforce algoritması

---
+++ src/com/bruteforce/Main.java
@@ -0,0 +1,35 @@
+package com.encryption;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int x = 0;
+        while (x < 1000) {
+            System.out.println(kodkampusu_sifreolusturma(5));
+            x++;
+        }
+    }
+
+    static char[] kodkampusu_sifreolusturma(int uzunlukdegeri) {
+        String karakterler = "ela";
+
+        String deger = karakterler;
+
+        char[] sifre = new char[uzunlukdegeri];
+        int i;
+        for (i = 0; i < uzunlukdegeri; i++) {
+            Random rastgelesifre = new Random();
+            sifre[i] += deger.charAt(rastgelesifre.nextInt(deger.length()));
+        }
+        System.out.print("Oluşturulan şifre = ");
+        return sifre;
+    }
+
+}
+
+
+
+
