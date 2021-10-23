c31baa687101f0db45d2d8753f78fbc5fcf4fbd3
hasan demir <65541711+hasandmr@users.noreply.github.com>
2021-10-23 22:20:45 +0300

    Update Main.java

--- src/com/bruteforce/Main.java
+++ src/com/bruteforce/Main.java
@@ -1,35 +1,50 @@
-package com.encryption;
+package test;
 
+import java.io.*;
+import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) {
-        int x = 0;
-        while (x < 1000) {
-            System.out.println(kodkampusu_sifreolusturma(5));
-            x++;
+        System.out.println("Bruteforce oluşturacak kelimeleri giriniz !!! :  ");
+            //burda random şifreler üretiliyor 
+
+        Scanner input = new Scanner(System.in);
+        String r = input.next();
+        String ç = "";
+        ArrayList<String> liste = new ArrayList<>();
+        int b = r.length();
+        for (int v = 0; v < 20000; v++) {
+            for (int w = 0; w < 5; w++) {
+                Random rand = new Random();
+                int e = rand.nextInt(b);
+                String n = r.substring(e, e + 1);
+                System.out.print(n);
+                System.out.print("");
+                ç += n;
+            }
+            liste.add(ç);
+            ç = "";
+            System.out.println();
+        }
+         // oluşan şifreyi masaüstüne txt'ye atıyor
+        String dosyaYolu = "C:\\Users\\hasan\\Desktop//metin1.txt";    .
+
+        try {
+            FileWriter fw = new FileWriter(dosyaYolu);
+            PrintWriter writer = new PrintWriter(fw);
+            for (int index = 0; index < liste.size(); index++) {
+                writer.println(liste.get(index));
+            }
+            writer.close();
+
+        } catch (FileNotFoundException ex) {
+            System.out.println("Dosya Bulunamadı Hatası : " + ex);
+        } catch (IOException ioe) {
+            System.out.println("Giriş Hatası : " + ioe);
         }
     }
-
-    static char[] kodkampusu_sifreolusturma(int uzunlukdegeri) {
-        String karakterler = "fırat";
-
-        String deger = karakterler;
-
-        char[] sifre = new char[uzunlukdegeri];
-        int i;
-        for (i = 0; i < uzunlukdegeri; i++) {
-            Random rastgelesifre = new Random();
-            sifre[i] += deger.charAt(rastgelesifre.nextInt(deger.length()));
-        }
-        System.out.print("Oluşturulan şifre = ");
-        return sifre;
-    }
-
 }
-
-
 
 
